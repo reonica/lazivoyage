85d390afaca1d607625387fcdd4b70226d9b0317
Reonica <148539983+reonica@users.noreply.github.com>
2025-09-17 17:19:50 +0700

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,6 +2,7 @@
 
 // 1. Danh sách URL (dữ liệu)
 const urlList = [
+  { name: "Sống Cùng Người Tày - Tìm Hiểu Văn Hoá Tày", url: "https://lazivoyage.blogspot.com/2025/09/song-cung-nguoi-tay-tim-hieu-van-hoa-tay.html" },
   { name: "Lịch Họp Chợ Phiên Ở Miền Bắc", url: "https://lazivoyage.blogspot.com/2025/09/lich-hop-cho-phien-o-mien-bac.html" },
   { name: "Lịch Mùa Lúa Chín Trong Năm Ở Miền Bắc", url: "https://lazivoyage.blogspot.com/2025/09/lich-mua-lua-chin-trong-nam-o-mien-bac.html" },
   { name: "Ý Nghĩa Bộ Bát Bảo Chấp Kích Trong Văn Hoá Việt", url: "https://lazivoyage.blogspot.com/2025/08/y-nghia-bo-bat-bao-chap-kich-trong-van-hoa-viet.html" },
